2e45a70c1a38e8ee8089057aecdb3e299668f40d
yuma14 <yuma140902@gmail.com>
2023-04-01 01:15:15 +0900

    init

---
+++ gvim.bat
@@ -0,0 +1,5 @@
+REM pretends to be gVim in order to launch neovim with https://github.com/yekingyan/obsidian-open-in-other-editor
+REM by yuma140902
+REM 2023-04-01
+
+nvim-qt %1 %2 %3 %4 %5 %6 %7 %8 %9
